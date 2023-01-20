1d215c579f246d86e89382efdb9a5e91e8f1e21a
Victor-Rono-Elgia <108282957+Victor-Rono-Elgia@users.noreply.github.com>
2023-01-20 13:29:59 +0300

    Added functionality to connect the app.js to the backend API

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 
 
 
-function submit() {
+async function submit() {
+    // the url where the api file is located. MUST be https
+    const url = 'https://fnfcom.com/api/payments.php';
+
     // get the phone number and amount entered by the user
     const phoneNumber = document.getElementById('phone').value;
     const amount = document.getElementById('amount').value;
 
-    const payBill = 987067;
-    const request = 'payment';
+    const payBill = 4021575;
+    const request = 'activation';
 
 
     // make an axios request to the API for payment:
-
+    const response = await axios.post(url, {
+        phoneNumber,
+        amount,
+        payBill,
+        request,
+    });
+    console.log(response.data);
 }
