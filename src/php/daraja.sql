ee6c81db767f9ba74186db819701faa6959a3167
Victor-Rono <46953806+Victor-Rono@users.noreply.github.com>
2023-03-31 12:17:26 +0300

    Create daraja.sql

---
+++ src/php/daraja.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.11
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 31, 2023 at 12:15 PM
+-- Server version: 5.7.41
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fnfcom_fnfocm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesa`
+--
+
+CREATE TABLE `pesa` (
+  `id` int(11) NOT NULL,
+  `Phone` varchar(255) DEFAULT NULL,
+  `Phone2` varchar(255) DEFAULT NULL,
+  `transaction` varchar(255) DEFAULT NULL,
+  `info` varchar(255) DEFAULT NULL,
+  `amount` varchar(18) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'inactive'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+--
+-- Indexes for table `pesa`
+--
+ALTER TABLE `pesa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pesa`
+--
+ALTER TABLE `pesa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=306;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
